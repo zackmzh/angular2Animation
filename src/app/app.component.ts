2767afbc6cce9c3310289b08e0d7efb36c3c298d
RICO MAI <ricomai@ricos-mac.local>
2017-09-18 10:06:29 -0400

    toggle light animation for a room

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component,
+         trigger,
+         state,
+         animate,
+         style,
+         transition,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations:[
+    trigger('lightOnOff',[
+      state('off',style({
+        backgroundColor:'black'
+      })),
+      state('on',style({
+        backgroundColor:'white'
+      })),
+      transition('off=>on',[animate('2s')]),
+      transition('on=>off',[animate('2s')])
+    ])
+  ]
 })
 export class AppComponent {
   title = 'app';
+  roomState : string = 'off';
+
+  change(){
+    this.roomState = (this.roomState==='off') ? "on" : "off";
+  }
 }
